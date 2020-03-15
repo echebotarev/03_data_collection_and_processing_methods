ff7ab876fe218e54ee2fcfb1be293828706a68b0
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-03-15 13:49:44 +0300

    Имплементация базы данных

--- HW_02/main.py
+++ HW_02/main.py
@@ -1,9 +1,22 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 import requests
 from pprint import pprint
+
+from HW_03.Mongo import Mongo
+mongo = Mongo()
 
 from Vacancies import Vacancies
 
 vacancy = input('Введите вакансию: ')
 
 v = Vacancies(vacancy)
-pprint(v.get())
+
+vacancies = v.get()
+pprint(vacancies)
+
+mongo.set_many(vacancies)
+
+for job in mongo.get(100000):
+  pprint(job)
