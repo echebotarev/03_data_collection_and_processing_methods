7146ef96f12562e8256a10b7edd7d5770689f374
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-03-27 15:34:42 +0300

    Парсинг авито - чтобы было

--- HW_06_avito/runner.py
+++ HW_06_avito/runner.py
@@ -1,16 +1,13 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 
-import sys
-print(sys.executable)
-
-from jobparser import settings
-from jobparser.spiders.hhru import HhruSpider
-from jobparser.spiders.sjru import SjruSpider
+from avito_parser.spiders.avito import AvitoSpider
+from avito_parser import settings
 
 if __name__ == '__main__':
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
     process = CrawlerProcess(settings=crawler_settings)
-    process.crawl(HhruSpider)
+    # input()
+    process.crawl(AvitoSpider, search='диван')
     process.start()
